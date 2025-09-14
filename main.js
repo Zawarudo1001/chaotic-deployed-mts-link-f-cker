5365ccf03867cf96b2749c4e2afcb4e0cb90b7c7
Zawarudo1001 <timohinvaceslav4@gmail.com>
2025-09-14 22:51:24 +0300

    реализована логика без воркера

---
+++ main.js
@@ -0,0 +1,22 @@
+(function() {
+    console.log('Hello from mts fixer');
+    
+    var originalFetch = window.fetch;
+    window.fetch = function(req, init) {
+        if (req.includes('/setUserInvolvementStatus')) {
+            init.body = 'isFocused=true&isSoundEnabled=true&isVideoEnabled=true';
+            return originalFetch(req, init);
+        }
+        return originalFetch.apply(window, arguments);
+    };
+
+    setInterval(function () {
+        let button =  Array.from(document.querySelectorAll('button')).find(button => button.textContent.trim() === 'Подтверждаю');
+        if (button) {
+            button.click();
+            console.log('Activity button clicked');
+        }
+        let watermark = document.querySelector('.WatermarkLogo__text___zmRdC');
+        if (watermark) watermark.textContent = 'Платформа 9¾'
+    }, 5000);
+})();
